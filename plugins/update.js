fca41da189f33f10b0db4763f03bea92aa2d9fe6
Aron <86765212+Chifuy@users.noreply.github.com>
2021-09-12 14:53:55 +0700

    Add files via upload

---
+++ plugins/update.js
@@ -0,0 +1,15 @@
+let { execSync } = require('child_process')
+let handler = async (m, { conn, text, isROwner }) => {
+  if (global.conn.user.jid == conn.user.jid) {
+    let stdout = execSync('git remote set-url origin https://github.com/Nurutomo/wabot-aq.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
+    m.reply(stdout.toString())
+  }
+}
+handler.help = ['update']
+handler.tags = ['host']
+handler.command = /^update$/i
+handler.rowner = true
+
+module.exports = handler
+
---
+++ plugins/waifu.js
@@ -0,0 +1,15 @@
+let fetch = require('node-fetch')
+let handler = async(m, { conn }) => {
+  m.reply('Searching...')
+  let res = await fetch('https://api.waifu.pics/sfw/waifu')
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'Istri Kartun :v', m)
+}
+handler.help = ['waifu']
+handler.tags = ['weeaboo']
+handler.limit = true
+handler.command = /^(waifu)$/i
+//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
+module.exports = handler
