78a03a04e41c30d450cde7ad5eb8b3d017803aed
Aron <86765212+Chifuy@users.noreply.github.com>
2021-09-15 20:19:07 +0700

    Update clear2.js

--- plugins/clear2.js
+++ plugins/clear2.js
@@ -1,5 +1,6 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'DELETE CHAT')).buffer(), `
+let image = 'https://telegra.ph/file/b277fb63f78d5a51fbbf6.jpg'
+let handler = async (m, { conn }) => conn.send3ButtonLoc(m.chat, await (await fetch(image)).buffer(), `
 Mau hapus chat apa..?
 `.trim(), '', 'CHAT GROUP', '!deletechat group', 'CHAT PRIVATE', '!deletechat chat', 'CHAT ALL', '!deletechat all', m)
 handler.help = ['dlt']
