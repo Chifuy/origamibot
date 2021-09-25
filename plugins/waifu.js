ad487759678457464fc659bc55c18ae98c19af54
Aron <86765212+Chifuy@users.noreply.github.com>
2021-09-25 18:33:18 +0700

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, usedPrefix }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), 'Istri Kok 2D :v', '', '🔍 Search WAIFU', `${usedPrefix}waifu, m)
+  await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), 'Istri Kok 2D :v', '', '🔍 Search WAIFU', `${usedPrefix}waifu`, m)
 }
 handler.help = ['waifu']
 handler.tags = ['weeaboo']
